ac722953110952ea0a80c6922b271e9dc614fceb
Manny Perez <mannyperez@Mannys-MacBook-Pro.local>
2017-01-11 17:22:56 -0600

    Mod to media files to show interpolation and expression content

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -5,4 +5,12 @@ selector: 'mw-media-item',
 templateUrl: 'app/media-item.component.html',
 styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent { }
+export class MediaItemComponent {
+
+  name = 'The Redemption';
+
+  wasWatched(){
+    return true;
+  }
+
+}
