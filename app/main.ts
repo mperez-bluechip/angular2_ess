628c30d0439ba9671019900c98d696d6d11d4a5b
Manny Perez <mannyperez@Mannys-MacBook-Pro.local>
2017-01-11 15:04:09 -0600

    Test commit

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
 import { platform@BrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule); 
