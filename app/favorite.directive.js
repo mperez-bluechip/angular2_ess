38c92ecb68a8f897676f9615642037cdae5733cb
Manny Perez <threedyou@gmail.com>
2017-01-12 15:33:33 -0600

    Mods to favorite and media files with directive setting and value

--- app/favorite.directive.js
+++ app/favorite.directive.js
@@ -22,10 +22,22 @@ System.register(['@angular/core'], function(exports_1, context_1) {
                 function FavoriteDirective() {
                     this.isFavorite = true;
                 }
+                Object.defineProperty(FavoriteDirective.prototype, "mwFavorite", {
+                    set: function (value) {
+                        this.isFavorite = value;
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
                 __decorate([
                     core_1.HostBinding('class.is-favorite'), 
                     __metadata('design:type', Object)
                 ], FavoriteDirective.prototype, "isFavorite", void 0);
+                __decorate([
+                    core_1.Input(), 
+                    __metadata('design:type', Object), 
+                    __metadata('design:paramtypes', [Object])
+                ], FavoriteDirective.prototype, "mwFavorite", null);
                 FavoriteDirective = __decorate([
                     core_1.Directive({
                         selector: '[mwFavorite]'
