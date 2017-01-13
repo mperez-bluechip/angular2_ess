0b1115399da932d12e24a6e30e618603557346cc
Manny Perez <threedyou@gmail.com>
2017-01-13 15:21:39 -0600

    Revert "Mods to app module file to bring in pipe category with category files added"
    This reverts commit 1e21df0cc074b6c9b75e5e46d1a416aad71bcbd2.

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,8 +6,11 @@ import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
+<<<<<<< HEAD
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
+=======
+>>>>>>> parent of 1e21df0... Mods to app module file to bring in pipe category with category files added
 
 @NgModule({
   imports: [
@@ -17,10 +20,15 @@ import { MediaItemFormComponent } from './media-item-form.component';
   declarations: [
     AppComponent,
     MediaItemComponent,
+<<<<<<< HEAD
     MediaItemListComponent,
     FavoriteDirective,
     CategoryListPipe,
     MediaItemFormComponent
+=======
+    MediaItemListComponent
+    FavoriteDirective
+>>>>>>> parent of 1e21df0... Mods to app module file to bring in pipe category with category files added
   ],
   bootstrap: [
     AppComponent
