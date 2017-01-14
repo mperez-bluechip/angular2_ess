79e9fd97ce98ccf1e26d622d157ee3821c77e68c
Manny Perez <threedyou@gmail.com>
2017-01-14 12:02:39 -0600

    Mods to media items files and service files setting up http backend reqs

--- app/media-item.service.ts
+++ app/media-item.service.ts
@@ -1,6 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable();
 export class MediaItemService {
+  constructor(private http: Http) {}
+
   get() {
-    return this.mediaItems;
+    return this.http.get('mediaitems')
+    .map(response  => {
+      return response.json().mediaItems;
+    });
   }
 
   add(mediaItem) {
